78db9584a691f98f0b232a75a6f99134a7cdb883
Alden Calatrava <calatravaalden@gmail.com>
2026-05-23 00:15:31 +0800

    Fix queue worker deploy skew causing incomplete class errors

--- tests/Feature/TestQueueCommandTest.php
+++ tests/Feature/TestQueueCommandTest.php
@@ -3,11 +3,27 @@
 namespace Tests\Feature;
 
 use App\Jobs\TestQueueJob;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Queue;
-use Tests\TestCase;
+use Mockery;
+use Tests\Support\CommandTestCase;
 
-class TestQueueCommandTest extends TestCase
+class TestQueueCommandTest extends CommandTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $jobsQuery = Mockery::mock();
+        $jobsQuery->shouldReceive('count')->andReturn(0);
+
+        $failedJobsQuery = Mockery::mock();
+        $failedJobsQuery->shouldReceive('count')->andReturn(0);
+
+        DB::shouldReceive('table')->with('jobs')->andReturn($jobsQuery);
+        DB::shouldReceive('table')->with('failed_jobs')->andReturn($failedJobsQuery);
+    }
+
     public function test_queue_test_dispatches_job(): void
     {
         Queue::fake();
---
+++ tests/Support/CommandTestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Support;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+/**
+ * Base for command/feature tests that do not need database migrations.
+ */
+abstract class CommandTestCase extends BaseTestCase
+{
+}
